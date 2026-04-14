b6f5ada646822b3cc8de993d4956ec52c6727083
SarialBebeto <sarial.tsamo@outlook.de>
2026-04-14 14:39:56 +0200

    a dynamic dropdown menu added to the user UI

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class Reservation(Base):
     date = Column(String, index=True)
     time = Column(String, index=True)
     email = Column(String, index=True)
+    package_type = Column(String, nullable=True)
     paid = Column(Boolean, default=False)
     paypal_order_id = Column(String, unique=True, nullable=True)
 
